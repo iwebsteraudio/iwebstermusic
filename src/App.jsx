cd9751b8c9f314a7b9526a115e53f84beeaddbe5
iwebsteraudio <iwebster.audio@gmail.com>
2024-06-24 15:25:58 +0100

    installs Router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
 
 function App() {
   
@@ -11,13 +13,10 @@ function App() {
       iwebstermusic
     </h1>
     </header>
-    </ Nav>
-     <section className='flex flex-col text-1 font-sans bg-white'>
-        <p className='py-8 px-8 my-8 mx8 text-gray-900'>Hello! I'm &nbsp;
-         <b>Ian Webster,</b>
-         &nbsp; an acoustic guitarist and singer based in Greater Manchester. I'm available for weddings and events across the UK.</p>
-         <p className='px-8 my-8 mx8 text-gray-900'>I have been performing in bands as a bass-player and also as a solo acoustic player for over 20 years in various venues across the north-west.</p>
-      </section>
+    <Routes>
+      <Route path="/" element={<Home className="py-8 px-8 my-8 mx-8" />} />
+    </Routes>
+     
   </>
   )
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+const Home = ()=>{
+
+    return(
+        <>
+    <section className='flex flex-col text-1 font-sans bg-white'>
+        <p className='py-8 px-8 my-8 mx8 text-gray-900'>Hello! I'm &nbsp;
+         <b>Ian Webster,</b>
+         &nbsp; an acoustic guitarist and singer based in Greater Manchester. I'm available for weddings and events across the UK.</p>
+         <p className='px-8 my-8 mx8 text-gray-900'>I have been performing in bands as a bass-player and also as a solo acoustic player for over 20 years in various venues across the north-west.</p>
+      </section>
+      
+      </>
+      )
+}
+
+export default Home;
