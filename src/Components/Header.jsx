0aaeea4156f18a643cd5f03fe444ecb4d3e2c93e
iwebsteraudio <iwebster.audio@gmail.com>
2024-06-26 10:39:52 +0100

    removes interfering styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,15 @@
 import Logo from "./Logo";
 
-const Header = ()=>{
-    return (
-        <header className="bg-dark-bckground sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b border-gray-500 p-8">
-            <Logo />
-            <h1 className="text">iwebstermusic</h1>
-            <h2>acoustic guitarist and singer</h2>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header className="bg-dark-bckground sticky top-0 z-20 mx-auto w-full flex items-center justify-between border-b border-gray-500 p-8">
+      <Logo />
+      <div className="flex flex-col items-start">
+        <h1 className="text-xl font-bold">iwebstermusic</h1>
+        <h2 className="text-base">acoustic guitarist and singer</h2>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
