1ab22acb1c04013797273acce3b8f4d0f5695ce6
iwebsteraudio <iwebster.audio@gmail.com>
2024-07-03 16:38:48 +0100

    adds logic for skip buttons and a timeline bar

--- src/Components/Listen.jsx
+++ src/Components/Listen.jsx
@@ -1,20 +1,66 @@
-import {
-  PlayIcon,
-  StopCircle,
-  SkipBack,
-  SkipForward,
-  PlayCircle,
-  PauseIcon,
-} from "lucide-react";
-import React from "react";
-import { useState } from "react";
+import { PlayCircle, SkipBack, SkipForward, PauseIcon } from "lucide-react";
+import React, { useState, useEffect } from "react";
 import useSound from "use-sound";
-import AllOfMe from "../../sounds/songs/AllOfMe.wav";
 
+const songs = [
+  { title: "All Of Me", path: "../../sounds/songs/AllOfMe.wav" },
+  { title: "Big Sur", path: "../../sounds/songs/BigSur.wav" },
+  {
+    title: "Bridge Over Troubled Water",
+    path: "../../sounds/songs/BridgeOverTroubledWater.wav",
+  },
+];
 const Listen = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: "",
+  });
 
-  const [play, { pause, duration, sound }] = useSound(AllOfMe);
+  const [seconds, setSeconds] = useState();
+  const [play, { pause, stop, duration, sound }] = useSound(
+    songs[trackIndex].path,
+    {
+      onend: () => {
+        setIsPlaying(false);
+      },
+    }
+  );
+  
+  const formatTime = (num) => String(num).padStart(2, "0");
+  const sec = duration / 1000;
+  const min = Math.floor(sec / 60);
+  const secRemain = Math.floor(sec % 60);
+  const time = {
+    min: formatTime(min),
+    sec: formatTime(secRemain),
+  };
+
+  useEffect(() => {
+    if (sound) {
+      stop();
+      setIsPlaying(false);
+    }
+  }, [trackIndex, play, stop]);
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min: formatTime(min),
+          sec: formatTime(sec),
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
   const playingButton = () => {
     if (isPlaying) {
       pause();
@@ -24,29 +70,63 @@ const Listen = () => {
       setIsPlaying(true);
     }
   };
+  const nextTrack = () => {
+    pause();
+    setIsPlaying(false);
+    setTrackIndex((prevIndex) => (prevIndex + 1) % songs.length);
+    play();
+    setIsPlaying(true);
+  };
+
+  const prevTrack = () => {
+    pause();
+    setIsPlaying(false);
+    setTrackIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+  };
 
   return (
-    <div className="component rounded">
-      <h2>Playing Now:</h2>
-      <div>
-        <h3 className="title">All Of Me</h3>
+    
+      <div className="component rounded-10px bg-white w-1/4 max-w-600px m-4 mx-auto pb-8 border border-black">
+        <h2 className="p-8">Playing Now:</h2>
+        <div>
+          <h3 className="title pb-8">{songs[trackIndex].title}</h3>
+        </div>
+        <button className="playButton p-8" onClick={prevTrack}>
+          <SkipBack />
+        </button>
+        {!isPlaying ? (
+          <button className="playButton p-8" onClick={playingButton}>
+            <PlayCircle />
+          </button>
+        ) : (
+          <button className="playButton p-8" onClick={playingButton}>
+            <PauseIcon />
+          </button>
+        )}
+        <button className="playButton p-8" onClick={nextTrack}>
+          <SkipForward />
+        </button>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
       </div>
-      <button className="playButton">
-        <SkipBack />
-      </button>
-      {!isPlaying ? (
-        <button className="playButton" onClick={playingButton}>
-          <PlayCircle />
-        </button>
-      ) : (
-        <button className="playButton" onClick={playingButton}>
-          <PauseIcon />
-        </button>
-      )}
-      <button className="playButton">
-        <SkipForward />
-      </button>
-    </div>
+    
   );
 };
 
