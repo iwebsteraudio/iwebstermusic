8e44b9f2c5c6635e64440ae846f2c70c4f89e498
iwebsteraudio <iwebster.audio@gmail.com>
2024-07-05 22:46:26 +0100

    a little bit of styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-stone-200 sticky top-0 z-40 flex items-center justify-evenly border-b-4 border-gray-950 p-8">
       <div className="flex flex-col">
         <Link to="/" className="font-righteous text-4xl">iwebstermusic</Link>
-        <h2 className="text-center">acoustic guitarist and singer</h2>
+        <h2 className="text-center">acoustic guitarist, bass-player and singer</h2>
       </div>
       
     </header>
