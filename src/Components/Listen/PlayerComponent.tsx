04c85d4df36d0b455e91cc7d3b6652c46b6b7b83
iwebsteraudio <iwebster.audio@gmail.com>
2024-11-27 11:14:57 +0000

    useRef was declared but not used. Deleted

--- src/Components/Listen/PlayerComponent.tsx
+++ src/Components/Listen/PlayerComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import PlayerControls from "./PlayerControls";
 import TimeDisplay from "./TimeDisplay";
 import useSound from "use-sound";
